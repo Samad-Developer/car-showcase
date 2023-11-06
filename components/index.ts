0d3df124c5d139f366e7a4a8eb8af73af754dcca
Abdus samad <abdussamadkotha@gmail.com>
2023-11-06 16:09:21 +0500

    Navbar, heroSection and footer

---
+++ components/index.ts
@@ -0,0 +1,13 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import { Navbar } from "./Navbar";
+import Footer from "./Footer";
+
+
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer
+}
