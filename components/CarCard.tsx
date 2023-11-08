88830a9c9826823b686102539e0c31f0c55b9118
Abdus samad <abdussamadkotha@gmail.com>
2023-11-08 17:12:42 +0500

    CarDetails

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,7 +3,8 @@ import { useState } from "react"
 import Image from "next/image"
 import { CarProps } from "@/types"
 import { CustomButton } from "."
-import { calculateCarRent } from "@/utils"
+import { calculateCarRent, generateCarImageUrl } from "@/utils"
+import CarDetails from "./CarDetails"
 interface CarCardProps {
     car: CarProps
 }
@@ -29,7 +30,7 @@ const CarCard = ({ car }: CarCardProps) => {
             </span>
         </p>
         <div className="relative w-full h-40 my-3 object-contain">
-            <Image src='/hero.png' alt="car" fill priority className="object-contain"/>
+            <Image src={generateCarImageUrl(car)} alt="car" fill priority className="object-contain"/>
         </div>
         <div className="relative flex w-full mt-2">
             <div className="flex group-hover:invisible w-full justify-between text-gray">
@@ -62,6 +63,16 @@ const CarCard = ({ car }: CarCardProps) => {
                 />
             </div>
         </div>
+
+        {/* to do..... */}
+    
+        <CarDetails
+         isOpen={isOpen}
+         closeModel={() => setIsOpen(false)}
+         car={car}
+        />
+
+        
     </div>
   )
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,4 @@
+import { CarProps } from "@/types";
 
 export async function fetchCars() {
     const headers = {
@@ -5,7 +6,7 @@ export async function fetchCars() {
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
         headers: headers,
     })
  
@@ -23,3 +24,17 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
     return rentalRatePerDay.toFixed(0)
 }
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL('https://cdn.imagin.studio/getimage')
+    const { make, model, year } = car
+    url.searchParams.append("customer", 'hrjavascript-mastery');
+    url.searchParams.append("make", make);
+    url.searchParams.append("modelFamily", model.split(' ')[0]);
+    url.searchParams.append("zoomType", 'fullscreen');
+    url.searchParams.append("modelYear", `${year}`);
+    url.searchParams.append("angle", `${angle}`);
+
+    return `${url}`
+}
+
